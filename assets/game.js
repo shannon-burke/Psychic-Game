a42d4d1dab64b88466c5d55b85d3127a6594eeaa
Shannon <shannonburke.art@gmail.com>
2019-06-20 23:45:57 -0500

    added html, linked bootstrap, wrote if statements. Tried some variables for inserting text that don't seem to be w orking

---
+++ assets/game.js
@@ -0,0 +1,37 @@
+var computerLetters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var guessesSoFar = [];
+
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    var computerChoice = computerLetters[Math.floor(Math.random() * computerLetters.length)];
+
+    if (userGuess === computerChoice) {
+        wins++;
+        guessesLeft = 9;
+        guessesSoFar = [];
+    }
+
+    if (userGuess != computerChoice) {
+        guessesLeft--;
+        guessesSoFar.push(userGuess);
+    }
+
+    if (guessesLeft === 0) {
+        guessesLeft = 9;
+        losses++;
+        guessesSoFar = [];
+    }
+
+    var actualGuessesSoFar = document.getElementById("guesses-so-far");
+    actualGuessesSoFar.textContent += guessesSoFar;
+
+    var actualGuessesLeft = document.getElementById("guesses-left");
+    actualGuessesLeft.textContent += guessesLeft;
+}
