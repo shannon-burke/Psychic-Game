df1789636e2c2b39a16eb1c6bf174d5148ca56f8
Shannon <shannonburke.art@gmail.com>
2019-06-21 00:52:24 -0500

    changed from inline to external JS doc

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,8 @@
-var computerLetters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+var guessesLeft = 9;
 var guessesSoFar = [];
 
 
@@ -21,6 +21,8 @@ document.onkeyup = function (event) {
     if (userGuess != computerChoice) {
         guessesLeft--;
         guessesSoFar.push(userGuess);
+
+
     }
 
     if (guessesLeft === 0) {
@@ -29,19 +31,40 @@ document.onkeyup = function (event) {
         guessesSoFar = [];
     }
 
-    /* commenting out bc it's not working and trying something else.
+    /*
+    document.getElementById("#guesses-so-far").innerHTML = guessesSoFar;
+    document.getElementById("#guesses-left").innerHTML = guessesLeft;
+    
 
-     var actualGuessesSoFar = document.getElementById("guesses-so-far");
-     actualGuessesSoFar.textContent = guessesSoFar;
- 
-     var actualGuessesLeft = document.getElementById("guesses-left");
-     actualGuessesLeft.textContent = guessesLeft;
-     
- This also doesn't work, ugh.
-    document.getElementById("guesses-so-far").innerHTML = guessesSoFar;
-    document.getElementById("guesses-left").innerHTML = guessesLeft;
 
-    console.log(guessesLeft);
+
+    var actualGuessesSoFar = document.getElementById("#guesses-so-far");
+    actualGuessesSoFar.textContent = guessesSoFar;
+
+    var actualGuessesLeft = document.getElementById("#guesses-left");
+    actualGuessesLeft.textContent = guessesLeft;
+
+
+    
+    actualGuessesSoFar = document.getElementById("guesses-so-far");
+    txt = document.createTextNode("your cool text");
+    span.appendChild(txt);
 
     */
+
+    //The console logs below are to test to make sure my actual function works. Which, at this point, it seems to be!!
+    //inserting the HTML is still not working yet.
+    console.log(wins);
+    console.log(losses);
+    console.log(guessesLeft);
+    console.log(guessesSoFar);
+
+
+    //I finally got something to work! I wanted to add spaces between the guesses so far, but couldn't get it to work. 
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("guesses-so-far").innerHTML = " " + guessesSoFar;
+
+
 }
